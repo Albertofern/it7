0165813f8634933150d476761817aec5cbc66ed5
Antonio Jota <antoniojherrera@localhost>
2018-05-21 18:40:49 +0200

    Funcion para obtener los puntos de un usuario especifico segun su id

--- Proyecto final/UpoCar/src/java/modelo/PuntuacionDAO.java
+++ Proyecto final/UpoCar/src/java/modelo/PuntuacionDAO.java
@@ -6,10 +6,40 @@
 
 package modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.core.GenericType;
+import webServiceREST.JerseyClientPuntuacion;
+import webServiceREST.entidades.Puntuacion;
+
 /**
  * 
  * @author Antonio Jose Herrera Tabaco 
  */
 public class PuntuacionDAO {
 
+    JerseyClientPuntuacion puntClient = new JerseyClientPuntuacion();
+    
+    public double getPuntuacionUsuario(int id){
+        GenericType<List<Puntuacion>> genericType = new GenericType<List<Puntuacion>>(){};
+            
+            List<Puntuacion> p = puntClient.findAll_XML(genericType);
+            
+            List<Puntuacion> listadoPuntuacionUsuarios = new ArrayList<Puntuacion>();
+            
+            int totalPuntuacion = 0;
+            
+            for(int i = 0; i < p.size(); i++){
+                Puntuacion punto = p.get(i);
+                
+                if(punto.getIdUsuarioRecibe().getIdUsuario() == id){ //puntuaciones del usuario con el id 1
+                    listadoPuntuacionUsuarios.add(punto);
+                    totalPuntuacion += punto.getPuntuacion();
+                }
+            }
+            
+            double puntosUser = totalPuntuacion / listadoPuntuacionUsuarios.size();
+            
+            return puntosUser;
+    }
 }
