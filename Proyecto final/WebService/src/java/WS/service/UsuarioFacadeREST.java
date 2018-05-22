359712115a4165f8b17da41da2e71254b0c30a63
gdelrio87 <gdelrioala@gmail.com>
2018-05-22 21:11:40 +0200

    WS - añadido buscar Usuario

--- Proyecto final/WebService/src/java/WS/service/UsuarioFacadeREST.java
+++ Proyecto final/WebService/src/java/WS/service/UsuarioFacadeREST.java
@@ -106,5 +106,22 @@ public class UsuarioFacadeREST extends AbstractFacade<Usuario> {
         
         return u;
     }
+    
+    @GET
+    @Path("/buscarUsuario/{nomUsuario}")
+    @Produces({MediaType.APPLICATION_XML})
+    public Usuario buscarUsuario(@PathParam("nomUsuario") String usuario){
+        //Usuario u = (Usuario) em.createNamedQuery("Usuario.login").setParameter("nomUsuario", usuario).getSingleResult();        
+        Query q =  em.createQuery("SELECT u FROM Usuario u WHERE u.nomUsuario = :nomUsuario").setParameter("nomUsuario", usuario);      
+        List l = q.getResultList();
+        
+        Usuario u = null;
+        
+        if(l.size() > 0){
+            u = (Usuario) l.get(0);
+        }
+        
+        return u;
+    }
 
 }
