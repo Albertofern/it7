6d978b3d670ae61696fcfeb14cad6cad493c3694
Antonio Jota <antoniojherrera@MacBook-Air-de-Antonio.local>
2018-06-06 16:20:42 +0200

    Metodo para obtener las fotos

--- Proyecto final/UpoCar/src/java/modelo/FotoVehiDAO.java
+++ Proyecto final/UpoCar/src/java/modelo/FotoVehiDAO.java
@@ -6,10 +6,33 @@
 
 package modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.core.GenericType;
+import webServiceREST.JerseyClientFotoVehi;
+import webServiceREST.entidades.FotoVehi;
+
+
 /**
  * 
  * @author Antonio Jose Herrera Tabaco 
  */
 public class FotoVehiDAO {
-
+    private JerseyClientFotoVehi cliente = new JerseyClientFotoVehi();
+    
+    public List<FotoVehi> fotosVehiculo (String id){
+        GenericType<List<FotoVehi>> genericType = new GenericType<List<FotoVehi>>() {
+        };
+        
+        List<FotoVehi> listadoFotos = cliente.findAll_XML(genericType);
+        List<FotoVehi> fotosVehiculo = new ArrayList<FotoVehi>();
+        
+        for (FotoVehi f : listadoFotos) {
+            if(f.getIdVehiculo().getIdVehiculo() == Integer.parseInt(id)){
+                fotosVehiculo.add(f);
+            }
+        }
+        
+        return fotosVehiculo;
+    }
 }
