ae791b609c4298aa12dea2dbee7d9a185e621c3a
Antonio Jota <antoniojherrera@localhost>
2018-06-08 19:11:55 +0200

    Validaciones de publicar viaje terminadas

--- Proyecto final/UpoCar/src/java/acciones/publicarViajeAction.java
+++ Proyecto final/UpoCar/src/java/acciones/publicarViajeAction.java
@@ -12,6 +12,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 import modelo.LocalidadDAO;
 import modelo.VehiculoDAO;
 import modelo.ViajeDAO;
@@ -25,20 +26,29 @@ import webServiceREST.entidades.Viaje;
  * @author Antonio Jose Herrera Tabaco
  */
 public class publicarViajeAction extends ActionSupport {
-    
+
     private String localidadOrigen;
     private String localidadDestino;
     private List origen;
-    private List destino;    
+    private List destino;
     private int plazas;
     private double precio;
     private String recogida;
     private String fechaSalida;
     private int coches;
+    private List<Vehiculo> listadoCoches;
 
     ViajeDAO dao = new ViajeDAO();
     VehiculoDAO vehiculoDao = new VehiculoDAO();
     LocalidadDAO localidadDao = new LocalidadDAO();
+
+    public List<Vehiculo> getListadoCoches() {
+        return listadoCoches;
+    }
+
+    public void setListadoCoches(List<Vehiculo> listadoCoches) {
+        this.listadoCoches = listadoCoches;
+    }
 
     public String getLocalidadOrigen() {
         return localidadOrigen;
@@ -55,7 +65,7 @@ public class publicarViajeAction extends ActionSupport {
     public void setLocalidadDestino(String localidadDestino) {
         this.localidadDestino = localidadDestino;
     }
-    
+
     public List getOrigen() {
         return origen;
     }
@@ -111,37 +121,77 @@ public class publicarViajeAction extends ActionSupport {
     public void setCoches(int coches) {
         this.coches = coches;
     }
-    
+
     public publicarViajeAction() {
     }
-    
-    public String crearViaje() throws ParseException{
+
+    public String crearViaje() throws ParseException {
         Map sesion = (Map) ActionContext.getContext().get("session");
         Usuario u = (Usuario) sesion.get("usuario");
-        
+
         //la fecha la guarda en el siguiente formato: 2018-05-30T14:15
         Date date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm").parse(fechaSalida);
         String formattedDate = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(date);
-        
+
         Viaje v = new Viaje(null, recogida, plazas, precio, formattedDate);
         v.setIdUsuarioPublica(u);
-                
+
         Vehiculo vehiculoSeleccionado = vehiculoDao.getVehiculoPorId(String.valueOf(coches));
         v.setIdVehiculoElegido(vehiculoSeleccionado);
-        
+
         //devolver la localidad por el origen
         Localidad lo = localidadDao.getLocalidadPorId(localidadOrigen);
         v.setIdLocalidadOrigen(lo);
         //devolver la locaidad por el destino
         Localidad ld = localidadDao.getLocalidadPorId(localidadDestino);
         v.setIdLocalidadDestino(ld);
-        
+
         dao.crearViaje(v);
         return SUCCESS;
     }
-    
+
     public String execute() throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
+
+    public void validate() {
+        
+        if(!Pattern.matches("[0-9]*", String.valueOf(plazas))){
+            addFieldError("plazas", "Debe introducir un numero");
+        }
+        
+        if(plazas <= 0){
+            addFieldError("plazas", "Debe introducir el numero de plazas");
+        }
+        
+        if(coches != 0 && plazas > vehiculoDao.getVehiculoPorId(String.valueOf(coches)).getPlazas()){
+            addFieldError("plazas", "El coche seleccionado no tiene plazas suficientes");
+        }
+        
+        if(!Pattern.matches("[0-9]*", String.valueOf(precio))){
+            addFieldError("precio", "Debe introducir un numero");
+        }
+        
+        if(coches == 0){
+            addFieldError("coches", "Debe seleccionar un coche");
+        }
+         
+        
+        if(fechaSalida.length() == 0){
+            addFieldError("recogida", "Debe introducir una fecha de salida");
+        }
+
+        if (recogida.length() == 0) {
+            addFieldError("fechaSalida", "Debe introducir un punto de recogida");
+        }
+        
+        origen = localidadDao.getLocalidades();
+        destino = localidadDao.getLocalidades();
+        
+        Map sesion = (Map) ActionContext.getContext().get("session");
+        Usuario u = (Usuario) sesion.get("usuario");
+
+        listadoCoches = vehiculoDao.listadoVehiculosUsuario(u.getIdUsuario());
+    }
+
 }
--- Proyecto final/UpoCar/web/vistas/publicarviaje.jsp
+++ Proyecto final/UpoCar/web/vistas/publicarviaje.jsp
@@ -7,18 +7,25 @@
                 <!-- Poner otro select para la provincia ? No seria dinamico al seleccionar=problema -->
 
                 Origen<s:select list="origen" name="localidadOrigen" listKey="idLocalidad" listValue="nombre" cssClass="form-control"></s:select><br/>
-                Destino<s:select list="destino" name="localidadDestino" listKey="idLocalidad" listValue="nombre" cssClass="form-control"></s:select><br/>
+                Destino<s:select list="destino" name="localidadDestino" listKey="idLocalidad" listValue="nombre" cssClass="form-control"></s:select><br/> 
                 
+                <s:fielderror fieldName="plazas" cssClass="alert alert-danger" />
                 Plazas<s:textfield name="plazas" cssClass="form-control"></s:textfield><br/>
+                
+                <s:fielderror fieldName="precio" cssClass="alert alert-danger" />
                 Precio<s:textfield name="precio" cssClass="form-control"></s:textfield><br/>
+                
+                <s:fielderror fieldName="recogida" cssClass="alert alert-danger" />
                 Punto de recogida<s:textarea name="recogida" cols="35" rows="8" cssClass="form-control"/><br/>
+                
+                <s:fielderror fieldName="fechaSalida" cssClass="alert alert-danger" />
                 Fecha y hora de salida<input id="datetime" name="fechaSalida" type="datetime-local" class="form-control"><br/>
 
             </div>
             <div class="col-lg-6">
                 <fieldset>
                     <legend>Seleccione el coche para el viaje</legend>             
-
+                    <s:fielderror fieldName="coches" cssClass="alert alert-danger" />
                     <s:iterator value="listadoCoches" var="vehiculo">
                         <s:radio name="coches" listKey="idVehiculo" list="%{marca+' ' +modelo+' ' +color}"/><br>
                     </s:iterator>
